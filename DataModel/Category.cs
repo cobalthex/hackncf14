4dbb9988d046dd2f5134c7d2f3b2a0c502f35029
snugeall <sethn443@gmail.com>
2014-10-26 00:48:52 -0400

    WCF Service

---
+++ DataModel/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    class Category
+    {
+        public System.Guid id { get; set; }
+        public 
+    }
+}
---
+++ DataModel/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DataModel
+{
+    public class Item
+    {
+        public System.Guid id { get; set; }
+        public long sku { get; set; }
+        public string name { get; set; }
+        public bool isRecipe { get; set; }
+        public Category category { get; set; }
+    }
+}
